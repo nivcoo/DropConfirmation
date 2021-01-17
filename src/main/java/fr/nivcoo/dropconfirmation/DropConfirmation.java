318efe8fb5ccc3cdcc67dbd525bd4719f78d94f9
nivcoo <nico64800@live.fr>
2021-01-17 11:44:43 +0100

    first

---
+++ src/main/java/fr/nivcoo/dropconfirmation/DropConfirmation.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package fr.nivcoo.dropconfirmation;
+
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.nivcoo.dropconfirmation.events.PlayerDropItem;
+import fr.nivcoo.dropconfirmation.utils.Config;
+
+public class DropConfirmation extends JavaPlugin {
+	private static DropConfirmation INSTANCE;
+	private Config config;
+
+	@Override
+	public void onEnable() {
+		INSTANCE = this;
+		config = new Config(new File("plugins" + File.separator + "DropConfirmation" + File.separator + "config.yml"));
+		saveDefaultConfig();
+		getCommand("dp").setExecutor(new DropConfirmationCommand());
+		Bukkit.getPluginManager().registerEvents(new PlayerDropItem(), this);
+		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+		Bukkit.getConsoleSender().sendMessage("§7DropConfirmation §av" + this.getDescription().getVersion());
+		Bukkit.getConsoleSender().sendMessage("§aPlugin Enabled !");
+		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+	}
+
+	@Override
+	public void onDisable() {
+		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+		Bukkit.getConsoleSender().sendMessage("§7DropConfirmation §cv" + this.getDescription().getVersion());
+		Bukkit.getConsoleSender().sendMessage("§cPlugin Disabled !");
+		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+	}
+	
+	
+	public void reload() {
+		config.loadConfig();
+	}
+
+
+	public static DropConfirmation get() {
+		return INSTANCE;
+	}
+	
+	public Config getConfiguration() {
+		return config;
+	}
+}
---
+++ src/main/java/fr/nivcoo/dropconfirmation/events/PlayerDropItem.java
@@ -0,0 +1,73 @@
+package fr.nivcoo.dropconfirmation.events;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import fr.nivcoo.dropconfirmation.DropConfirmation;
+import fr.nivcoo.dropconfirmation.utils.Config;
+
+public class PlayerDropItem implements Listener {
+	
+	private HashMap<String, HashMap<ItemStack, Long>> wait = new HashMap<>();
+	private DropConfirmation dp = DropConfirmation.get();
+	private Config config = dp.getConfiguration();
+	int secondsBeforeReset = config.getInt("seconds_before_reset");
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerDropItemEvent(PlayerDropItemEvent e) {
+
+		Player p = e.getPlayer();
+		if (p.hasPermission("dropconfirmation.bypass"))
+			return;
+
+		Item i = e.getItemDrop();
+		ItemStack item = i.getItemStack();
+		boolean confirmOnlyName = config.getBoolean("confirm_only_renamed");
+		List<String> whitelistedItems = config.getStringList("whitelisted_items");
+		List<String> blacklistedWorlds = config.getStringList("blacklisted_world");
+		
+		if (blacklistedWorlds.contains(p.getWorld().getName()) || (!whitelistedItems.isEmpty() && !whitelistedItems.contains(item.getType().toString())) || (confirmOnlyName && !item.getItemMeta().hasDisplayName()))
+			return;
+		
+		HashMap<ItemStack, Long> items = wait.get(p.getUniqueId().toString());
+		Long currentMillis = System.currentTimeMillis();
+		if(items != null) {
+			Long millis = items.get(item);
+			if(millis == null) {
+				items.put(item, currentMillis);
+				sendCancelMessage(p);
+				e.setCancelled(true);
+				
+			} else {
+				if((currentMillis - millis) / 1000 > secondsBeforeReset) {
+					items.put(item, currentMillis);
+					sendCancelMessage(p);
+					e.setCancelled(true);
+				} else {
+					items.remove(item);
+				}
+			}
+			
+		} else {
+			items = new HashMap<>();
+			items.put(item, currentMillis);
+			sendCancelMessage(p);
+			e.setCancelled(true);
+		}
+		wait.put(p.getUniqueId().toString(), items);
+
+	}
+	
+	private void sendCancelMessage(Player p) {
+		p.sendMessage(config.getString("messages.prefix") + config.getString("messages.cancel_message").replace("{0}", String.valueOf(secondsBeforeReset)));
+	}
+
+}
