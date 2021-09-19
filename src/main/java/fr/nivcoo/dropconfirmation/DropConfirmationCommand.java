30e8b4a4a0d635bd64a42697a660720098c1f713
nivcoo <nico64800@live.fr>
2021-09-19 13:53:13 +0200

    improv. remove warnings

--- src/main/java/fr/nivcoo/dropconfirmation/DropConfirmationCommand.java
+++ src/main/java/fr/nivcoo/dropconfirmation/DropConfirmationCommand.java
@@ -1,47 +1,46 @@
 package fr.nivcoo.dropconfirmation;
 
+import fr.nivcoo.dropconfirmation.utils.Config;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import fr.nivcoo.dropconfirmation.utils.Config;
-
 public class DropConfirmationCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!"dp".equalsIgnoreCase(command.getLabel()))
-			return false;
-		DropConfirmation dp = DropConfirmation.get();
-		Config config = dp.getConfiguration();
-		String prefix = config.getString("messages.prefix");
-		String unknownCmd = config.getString("messages.no_permission");
-		if (args.length == 0 || "help".equalsIgnoreCase(args[0])) {
-			sender.sendMessage(
-					ChatColor.YELLOW + "-----------[" + ChatColor.GOLD + dp.getDescription().getName() + ChatColor.YELLOW + "]-----------");
-			sender.sendMessage(
-					ChatColor.AQUA + "- /dp info" + ChatColor.YELLOW + " : See informations about plugin");
-			if (sender.hasPermission("dropconfirmation.reload"))
-				sender.sendMessage(ChatColor.AQUA + "- /dp reload" + ChatColor.YELLOW + " : Reload the plugin");
-		} else if ("info".equalsIgnoreCase(args[0])) {
-			sender.sendMessage(
-					ChatColor.YELLOW + "-----------[" + ChatColor.GOLD + dp.getDescription().getName() + ChatColor.YELLOW + "]-----------");
-			sender.sendMessage(ChatColor.AQUA + "Created by nivcoo");
-			sender.sendMessage(ChatColor.GREEN + "v" + dp.getDescription().getVersion());
-			sender.sendMessage(ChatColor.AQUA + "https://www.nivcoo.fr");
-			sender.sendMessage(ChatColor.AQUA + "https://www.github.com/nivcoo");
-		} else if ("reload".equalsIgnoreCase(args[0])) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!"dp".equalsIgnoreCase(command.getLabel()))
+            return false;
+        DropConfirmation dp = DropConfirmation.get();
+        Config config = dp.getConfiguration();
+        String prefix = config.getString("messages.prefix");
+        String unknownCmd = config.getString("messages.no_permission");
+        if (args.length == 0 || "help".equalsIgnoreCase(args[0])) {
+            sender.sendMessage(
+                    ChatColor.YELLOW + "-----------[" + ChatColor.GOLD + dp.getDescription().getName() + ChatColor.YELLOW + "]-----------");
+            sender.sendMessage(
+                    ChatColor.AQUA + "- /dp info" + ChatColor.YELLOW + " : See informations about plugin");
+            if (sender.hasPermission("dropconfirmation.reload"))
+                sender.sendMessage(ChatColor.AQUA + "- /dp reload" + ChatColor.YELLOW + " : Reload the plugin");
+        } else if ("info".equalsIgnoreCase(args[0])) {
+            sender.sendMessage(
+                    ChatColor.YELLOW + "-----------[" + ChatColor.GOLD + dp.getDescription().getName() + ChatColor.YELLOW + "]-----------");
+            sender.sendMessage(ChatColor.AQUA + "Created by nivcoo");
+            sender.sendMessage(ChatColor.GREEN + "v" + dp.getDescription().getVersion());
+            sender.sendMessage(ChatColor.AQUA + "https://www.nivcoo.fr");
+            sender.sendMessage(ChatColor.AQUA + "https://www.github.com/nivcoo");
+        } else if ("reload".equalsIgnoreCase(args[0])) {
 
-			if (!sender.hasPermission("dropconfirmation.reload")) {
-				sender.sendMessage(unknownCmd);
-				return true;
-			}
-			sender.sendMessage(prefix + ChatColor.GREEN + "Reloading ...");
-			dp.reload();
-			sender.sendMessage(prefix + ChatColor.GREEN + "Reloaded");
-		}
-		return true;
-	}
+            if (!sender.hasPermission("dropconfirmation.reload")) {
+                sender.sendMessage(unknownCmd);
+                return true;
+            }
+            sender.sendMessage(prefix + ChatColor.GREEN + "Reloading ...");
+            dp.reload();
+            sender.sendMessage(prefix + ChatColor.GREEN + "Reloaded");
+        }
+        return true;
+    }
 
 }
--- src/main/java/fr/nivcoo/dropconfirmation/events/PlayerDropItem.java
+++ src/main/java/fr/nivcoo/dropconfirmation/events/PlayerDropItem.java
@@ -1,9 +1,7 @@
 package fr.nivcoo.dropconfirmation.events;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
+import fr.nivcoo.dropconfirmation.DropConfirmation;
+import fr.nivcoo.dropconfirmation.utils.Config;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,76 +12,77 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
-import fr.nivcoo.dropconfirmation.DropConfirmation;
-import fr.nivcoo.dropconfirmation.utils.Config;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class PlayerDropItem implements Listener {
 
-	private HashMap<String, HashMap<ItemStack, Long>> wait = new HashMap<>();
-	private DropConfirmation dp = DropConfirmation.get();
-	private Config config = dp.getConfiguration();
-	int secondsBeforeReset = config.getInt("seconds_before_reset");
+    private HashMap<String, HashMap<ItemStack, Long>> wait = new HashMap<>();
+    private DropConfirmation dp = DropConfirmation.get();
+    private Config config = dp.getConfiguration();
+    int secondsBeforeReset = config.getInt("seconds_before_reset");
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerDropItemEvent(PlayerDropItemEvent e) {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerDropItemEvent(PlayerDropItemEvent e) {
 
-		Player p = e.getPlayer();
-		ArrayList<InventoryType> it = new ArrayList<>();
-		it.add(InventoryType.CRAFTING);
-		it.add(InventoryType.PLAYER);
-		it.add(InventoryType.CREATIVE);
-		InventoryView iv = p.getOpenInventory();
+        Player p = e.getPlayer();
+        ArrayList<InventoryType> it = new ArrayList<>();
+        it.add(InventoryType.CRAFTING);
+        it.add(InventoryType.PLAYER);
+        it.add(InventoryType.CREATIVE);
+        InventoryView iv = p.getOpenInventory();
 
-		if (p.hasPermission("dropconfirmation.bypass") || (iv != null && !it.contains(iv.getType())))
-			return;
+        if (p.hasPermission("dropconfirmation.bypass") || (iv != null && !it.contains(iv.getType())))
+            return;
 
-		Item i = e.getItemDrop();
-		ItemStack item = i.getItemStack();
-		boolean confirmOnlyName = config.getBoolean("confirm_only_renamed");
-		List<String> whitelistedItems = config.getStringList("whitelisted_items");
-		List<String> blacklistedWorlds = config.getStringList("blacklisted_world");
-		if (blacklistedWorlds.contains(p.getWorld().getName())
-				|| (!whitelistedItems.isEmpty() && !whitelistedItems.contains(item.getType().toString()))
-				|| (confirmOnlyName && !item.getItemMeta().hasDisplayName()))
-			return;
-		boolean perItemConfirmation = config.getBoolean("per_item_confirmation");
-		boolean resetConfirmAfterDrop = config.getBoolean("reset_confirm_after_drop");
-		HashMap<ItemStack, Long> items = wait.get(p.getUniqueId().toString());
-		Long currentMillis = System.currentTimeMillis();
-		if (!perItemConfirmation)
-			item = null;
-		if (items != null) {
-			Long millis = items.get(item);
-			if (millis == null) {
-				items.put(item, currentMillis);
-				sendCancelMessage(p);
-				e.setCancelled(true);
+        Item i = e.getItemDrop();
+        ItemStack item = i.getItemStack();
+        boolean confirmOnlyName = config.getBoolean("confirm_only_renamed");
+        List<String> whitelistedItems = config.getStringList("whitelisted_items");
+        List<String> blacklistedWorlds = config.getStringList("blacklisted_world");
+        if (blacklistedWorlds.contains(p.getWorld().getName())
+                || (!whitelistedItems.isEmpty() && !whitelistedItems.contains(item.getType().toString()))
+                || (confirmOnlyName && !item.getItemMeta().hasDisplayName()))
+            return;
+        boolean perItemConfirmation = config.getBoolean("per_item_confirmation");
+        boolean resetConfirmAfterDrop = config.getBoolean("reset_confirm_after_drop");
+        HashMap<ItemStack, Long> items = wait.get(p.getUniqueId().toString());
+        Long currentMillis = System.currentTimeMillis();
+        if (!perItemConfirmation)
+            item = null;
+        if (items != null) {
+            Long millis = items.get(item);
+            if (millis == null) {
+                items.put(item, currentMillis);
+                sendCancelMessage(p);
+                e.setCancelled(true);
 
-			} else {
-				if ((currentMillis - millis) / 1000 > secondsBeforeReset) {
-					sendCancelMessage(p);
+            } else {
+                if ((currentMillis - millis) / 1000 > secondsBeforeReset) {
+                    sendCancelMessage(p);
 
-					e.setCancelled(true);
-				} else if (resetConfirmAfterDrop) {
-					items.remove(item);
-				}
-				items.put(item, currentMillis);
-			}
+                    e.setCancelled(true);
+                } else if (resetConfirmAfterDrop) {
+                    items.remove(item);
+                }
+                items.put(item, currentMillis);
+            }
 
-		} else {
-			items = new HashMap<>();
-			items.put(item, currentMillis);
-			sendCancelMessage(p);
-			e.setCancelled(true);
-		}
+        } else {
+            items = new HashMap<>();
+            items.put(item, currentMillis);
+            sendCancelMessage(p);
+            e.setCancelled(true);
+        }
 
-		wait.put(p.getUniqueId().toString(), items);
+        wait.put(p.getUniqueId().toString(), items);
 
-	}
+    }
 
-	private void sendCancelMessage(Player p) {
-		p.sendMessage(config.getString("messages.prefix")
-				+ config.getString("messages.cancel_message").replace("{0}", String.valueOf(secondsBeforeReset)));
-	}
+    private void sendCancelMessage(Player p) {
+        p.sendMessage(config.getString("messages.prefix")
+                + config.getString("messages.cancel_message").replace("{0}", String.valueOf(secondsBeforeReset)));
+    }
 
 }
